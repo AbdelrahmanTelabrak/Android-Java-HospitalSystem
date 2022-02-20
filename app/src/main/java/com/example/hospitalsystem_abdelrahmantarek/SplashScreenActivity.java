9e06aae02a75e2461e26bb50496f3eb5efc0dcd5
AbdelRahman <abdelrahmantelabrak@gmail.com>
2022-02-21 00:40:21 +0200

    day1 commit 2

--- app/src/main/java/com/example/hospitalsystem_abdelrahmantarek/SplashScreenActivity.java
+++ app/src/main/java/com/example/hospitalsystem_abdelrahmantarek/SplashScreenActivity.java
@@ -1,14 +1,51 @@
 package com.example.hospitalsystem_abdelrahmantarek;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.WindowManager;
+
+import com.example.hospitalsystem_abdelrahmantarek.StartUp.StartUpActivity;
 
 public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        hideSystemBars();
         setContentView(R.layout.activity_splash_screen);
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(2000);
+
+                    Intent intent = new Intent(SplashScreenActivity.this, StartUpActivity.class);
+                    startActivity(intent);
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        thread.start();
+    }
+    private void hideSystemBars() {
+        WindowInsetsControllerCompat windowInsetsController =
+                ViewCompat.getWindowInsetsController(getWindow().getDecorView());
+        if (windowInsetsController == null) {
+            return;
+        }
+        // Configure the behavior of the hidden system bars
+        windowInsetsController.setSystemBarsBehavior(
+                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        );
+        // Hide both the status bar and the navigation bar
+        windowInsetsController.hide(WindowInsetsCompat.Type.navigationBars());
     }
 }
