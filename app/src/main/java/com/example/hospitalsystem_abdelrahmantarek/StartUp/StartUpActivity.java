0d24523838f198e3c1dec8c567ba4f9da1e1abc4
AbdelRahman <abdelrahmantelabrak@gmail.com>
2022-02-20 22:44:29 +0200

    day1 commit 1

---
+++ app/src/main/java/com/example/hospitalsystem_abdelrahmantarek/StartUp/StartUpActivity.java
@@ -0,0 +1,16 @@
+package com.example.hospitalsystem_abdelrahmantarek.StartUp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.hospitalsystem_abdelrahmantarek.R;
+
+public class StartUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_up);
+    }
+}
