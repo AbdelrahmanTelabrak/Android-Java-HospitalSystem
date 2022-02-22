5995c91ab969af6f268c467323882f92502927ac
AbdelRahman <abdelrahmantelabrak@gmail.com>
2022-02-22 02:32:59 +0200

    day2 commit 1

--- app/src/main/java/com/example/hospitalsystem_abdelrahmantarek/StartUp/LoginFragment.java
+++ app/src/main/java/com/example/hospitalsystem_abdelrahmantarek/StartUp/LoginFragment.java
@@ -1,33 +1,35 @@
 package com.example.hospitalsystem_abdelrahmantarek.StartUp;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
 import com.example.hospitalsystem_abdelrahmantarek.R;
-import com.example.hospitalsystem_abdelrahmantarek.databinding.FragmentPrototypeMapBinding;
+import com.example.hospitalsystem_abdelrahmantarek.databinding.FragmentLoginBinding;
 
-public class PrototypeMapFragment extends Fragment {
 
-    FragmentPrototypeMapBinding binding;
+public class LoginFragment extends Fragment {
+
+    FragmentLoginBinding binding;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_prototype_map, container, false);
-        return  binding.getRoot();
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false);
+        return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+
     }
 }
--- app/src/main/java/com/example/hospitalsystem_abdelrahmantarek/StartUp/StartUpActivity.java
+++ app/src/main/java/com/example/hospitalsystem_abdelrahmantarek/StartUp/StartUpActivity.java
@@ -1,6 +1,9 @@
 package com.example.hospitalsystem_abdelrahmantarek.StartUp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
 
 import android.os.Bundle;
 
@@ -11,6 +14,23 @@ public class StartUpActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        hideSystemBars();
         setContentView(R.layout.activity_start_up);
+
+
+    }
+
+    private void hideSystemBars() {
+        WindowInsetsControllerCompat windowInsetsController =
+                ViewCompat.getWindowInsetsController(getWindow().getDecorView());
+        if (windowInsetsController == null) {
+            return;
+        }
+        // Configure the behavior of the hidden system bars
+        windowInsetsController.setSystemBarsBehavior(
+                WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        );
+        // Hide both the status bar and the navigation bar
+        windowInsetsController.hide(WindowInsetsCompat.Type.navigationBars());
     }
 }
